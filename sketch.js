f8a48316a0160e0451ea1cd0244782cfa307720b
love245 <129867336+love245@users.noreply.github.com>
2023-10-26 10:24:02 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -3,14 +3,44 @@
 function setup() {
   createCanvas(400,400);
   
+caixa = createSprite(200,200,30,30)
+
+
 }
 
 function draw() 
 {
-  background(30);
+  background('#4b0081');
 
 
   drawSprites();
+
+if(keyIsDown(LEFT_ARROW)){
+
+caixa.position.x -=5
+
+
+}
+if(keyIsDown(RIGHT_ARROW)){
+
+  caixa.position.x +=5
+  
+  
+  }
+  if(keyIsDown(UP_ARROW)){
+
+    caixa.position.y -=5
+    
+    
+    }
+    
+    if(keyIsDown(DOWN_ARROW)){
+
+      caixa.position.y +=5
+      
+      
+      }
+      
 
 }
 
